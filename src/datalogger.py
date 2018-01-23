fbe6e8f82341af517874cc2c5244cf009d3d21ec
jmFernandezMola <josep.maria.fernandez@gmail.com>
2018-01-23 00:58:18 -0800

    Fixed some typos

--- src/datalogger.py
+++ src/datalogger.py
@@ -6,39 +6,39 @@ import datetime
 import dropbox #Requereix dropbox
 
 
-def serialConnect():
-    serlocations=['/dev/ttyUSB0','/dev/ttyACM', '/dev/ttyACM0', '/dev/ttyACM1','/dev/ttyACM2', '/dev/ttyACM3','/dev/ttyACM4', '/dev/ttyACM5','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3', '/dev/ttyUSB4', '/dev/ttyUSB5', '/dev/ttyUSB6', '/dev/ttyUSB7', '/dev/ttyUSB8', '/dev/ttyUSB9', '/dev/ttyUSB10','/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', 'com2', 'com3', 'com4', 'com5', 'com6', 'com7', 'com8', 'com9', 'com10', 'com11', 'com12', 'com13', 'com14', 'com15', 'com16', 'com17', 'com18', 'com19', 'com20', 'com21', 'com1', 'end']
-    for device in serlocations:
-        try:
-            ser = serial.Serial(
-                port=device,
-                baudrate=19200,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS
-            )
-            print device
-            return ser
-        except:
-	    x=0
-    if device == 'end':
-        print "No Device Found"
+def serial_connect():
+	serlocations=['/dev/ttyUSB0','/dev/ttyACM', '/dev/ttyACM0', '/dev/ttyACM1','/dev/ttyACM2', '/dev/ttyACM3','/dev/ttyACM4', '/dev/ttyACM5','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3', '/dev/ttyUSB4', '/dev/ttyUSB5', '/dev/ttyUSB6', '/dev/ttyUSB7', '/dev/ttyUSB8', '/dev/ttyUSB9', '/dev/ttyUSB10','/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', 'com2', 'com3', 'com4', 'com5', 'com6', 'com7', 'com8', 'com9', 'com10', 'com11', 'com12', 'com13', 'com14', 'com15', 'com16', 'com17', 'com18', 'com19', 'com20', 'com21', 'com1', 'end']
+	for device in serlocations:
+		try:
+			ser = serial.Serial(
+				port=device,
+				baudrate=19200,
+				parity=serial.PARITY_NONE,
+				stopbits=serial.STOPBITS_ONE,
+				bytesize=serial.EIGHTBITS
+			)
+			print device
+			return ser
+		except:
+		x=0
+	if device == 'end':
+		print "No Device Found"
 
-ser = serialConnect()
+ser = serial_connect()
 data = ""
 if ser:
 #    ser.write("KNOCK KNOCK")
 # Wait until a msg is received
-    print "Escoltant el bus..."
-    while True:
-	time.sleep(1)
-	if (ser.inWaiting() > 0) :
-		data += ser.read(ser.inWaiting())
-		t1 = time.time()
-	if (data.__len__() > 0) :
-		timeSinceLastMessage = time.time() - t1
-		if timeSinceLastMessage > 2.0:
-			break;
+	print "Waiting communications..."
+	while True:
+		time.sleep(1)
+		if ser.inWaiting() > 0 :
+			data += ser.read(ser.inWaiting())
+			t1 = time.time()
+		if data.__len__() > 0 :
+			timeSinceLastMessage = time.time() - t1
+			if timeSinceLastMessage > 2.0:
+				break
 
 
 #  In the following lines the program will search the delimiters
@@ -50,11 +50,11 @@ dataShadowCopy = data
 setDelimiter = str(bytearray.fromhex('FF FF FF FF FF FF FF FF'))
 while  setDelimiter in dataShadowCopy:
 	h = dataShadowCopy.find(setDelimiter)
-    	dataShadowCopy = dataShadowCopy[h+8:] # Destroy all the data until the first delimiter
-    	if setDelimiter  in dataShadowCopy:
-        	listArrays[listIndex] = str(dataShadowCopy[:dataShadowCopy.find(setDelimiter)]) # Keep data in this interval
-    	else:
-       		listArrays[listIndex] = str(dataShadowCopy) # Keep data of the last interval
+	dataShadowCopy = dataShadowCopy[h+8:] # Destroy all the data until the first delimiter
+		if setDelimiter  in dataShadowCopy:
+			listArrays[listIndex] = str(dataShadowCopy[:dataShadowCopy.find(setDelimiter)]) # Keep data in this interval
+		else:
+			listArrays[listIndex] = str(dataShadowCopy) # Keep data of the last interval
 		print "Ha trobat algun missatge \n"
 	listIndex += 1
 
@@ -80,19 +80,19 @@ for x in range(len(listArrays)):
 print listValues
 
 #CSV and Dropbox part
-fileName = "logger_BBB_" + time.strftime("%Y%m%d_%H%M%S", time.gmtime()) + ".csv"
+fileName = "/logs/logger_BBB_" + time.strftime("%Y%m%d_%H%M%S", time.gmtime()) + ".csv"
 dbxPath = "/Datalogger"
 
 with open(fileName, 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=';')
-    spamwriter.writerow(["Data taken on", time.strftime("%a - %d %b %Y %H:%M:%S", time.gmtime())])
-    spamwriter.writerow(["By the Beagle Black Box"])
-    spamwriter.writerow("\n")
-    for x in range(len(listValues[0])):
-        spamwriter.writerow([elements[x] for elements in listValues])
+	spamwriter = csv.writer(csvfile, delimiter=';')
+	spamwriter.writerow(["Data taken on", time.strftime("%a - %d %b %Y %H:%M:%S", time.gmtime())])
+	spamwriter.writerow(["By the Beagle Black Box"])
+	spamwriter.writerow("\n")
+	for x in range(len(listValues[0])):
+		spamwriter.writerow([elements[x] for elements in listValues])
 
 with open(fileName, 'rb') as f:
-    	dataFile = f.read()
+	dataFile = f.read()
 
 with open("passcode.txt", 'r') as code:
 	pwd =  code.readline()
@@ -101,10 +101,10 @@ with open("passcode.txt", 'r') as code:
 
 dbx = dropbox.Dropbox(pwd) 
 try:
-    dbxPath += "/"+fileName
-    res = dbx.files_upload(dataFile, dbxPath)
+	dbxPath += "/"+fileName
+	res = dbx.files_upload(dataFile, dbxPath)
 except dropbox.exceptions.ApiError as err:
-    print('*** API error', err)
+	print('*** API error', err)
 print dbxPath
 print('uploaded as', res.name.encode('utf8'))
 print ('Total byte count for each field ',desideredLength)
